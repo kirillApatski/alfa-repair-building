0a0a7b75b6136555032f889683cb075e28678c77
Kirill <apatski94@gmail.com>
2023-04-03 19:36:38 +0300

    added images and component Svg

---
+++ src/assets/iconsSVG/FooterLogoSvg.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const FooterLogoSvg = () => {
+  return (
+
+    <svg width="240" height="61" viewBox="0 0 240 61" fill="none" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
+      <rect width="240" height="61" fill="url(#pattern0)"/>
+      <defs>
+        <pattern id="pattern0" patternContentUnits="objectBoundingBox" width="1" height="1">
+          <use xlinkHref="#image0_4_452" transform="matrix(0.00416667 0 0 0.0163934 0 -0.565574)"/>
+        </pattern>
+      </defs>
+    </svg>
+
+  );
+};
+
+export default FooterLogoSvg;
