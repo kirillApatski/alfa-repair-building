c6f0d92f0541f3d4425b7f616db3d74dfcb7c689
Kirill <apatski94@gmail.com>
2023-04-03 19:38:16 +0300

    added feedback section

---
+++ src/components/CasePage/Feedback/Feedback.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import styles from './Feedback.module.scss'
+
+const Feedback = () => {
+  return (
+    <section className={styles.feedBackHomeWrapper}>
+      <div className={`${styles.content} container`}>
+        <form className={styles.feedbackForm}>
+          <h2 className={styles.title}>Хотите с нами сотрудничать?</h2>
+          <p className={styles.description}>Заполните форму и мы перезвоним вам в течение 30 минут!</p>
+          <input className={styles.formItem} type='text' placeholder='Ваше имя'/>
+          <input className={styles.formItem} type='number' placeholder='Номер телефона'/>
+          <textarea className={styles.feedBackMessages} placeholder='Ваше сообщение (необязательно)'></textarea>
+          <div className={styles.btnBox}>
+            <button className={styles.feedBackBtn}>Отправить</button>
+          </div>
+        </form>
+      </div>
+    </section>
+  );
+};
+
+export default Feedback;
